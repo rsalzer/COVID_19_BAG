90dffc37815fa45cae74b499baf0472456a28e31
Robert Salzer <robert.salzer@garzotto.com>
2020-07-17 00:11:49 +0200

    update scraper

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -31,10 +31,6 @@ const bagExcelLocation = "https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und
 const ageLabels = ["0-9","10-19","20-29","30-39","40-49","50-59","60-69","70-79","80+"];
 var appendToFiles = false;
 
-console.log("The Test: " +  process.env.TEST);
-
-tweet("Test-Tweet frmo github actions");
-
 var myArgs = process.argv.slice(2);
 switch (myArgs[0]) {
 case 'download':
