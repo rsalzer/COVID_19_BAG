d591a3cee1a7c3598e6bf513a97ba1e500a5fc5a
Robert Salzer <robert.salzer@garzotto.com>
2021-01-09 18:22:51 +0100

    deploy vergleich

--- docs/script.js
+++ docs/script.js
@@ -454,3 +454,32 @@ app.controller('ChartCtrl', ['$scope', function ($scope) {
   }
 
 }]);
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("nav");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("url");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.insertAdjacentHTML("afterbegin", this.responseText);}
+          if (this.status == 404) {elmnt.insertAdjacentHTML("afterbegin", "Page not found.");}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("url");
+          document.getElementById("pagenav_r").className = "here";
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+includeHTML();
