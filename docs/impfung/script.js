928b2f514d00096650e0023b1f0a7a3d0633b87a
Robert Salzer <robert.salzer@garzotto.com>
2021-01-30 20:37:14 +0100

    adjust header to include impfung

--- docs/impfung/script.js
+++ docs/impfung/script.js
@@ -47,7 +47,7 @@ function includeHTML() {
     }
   }
 }
-//includeHTML();
+includeHTML();
 
 const cantons = ['AG', 'AI', 'AR', 'BE', 'BL', 'BS', 'FR', 'GE', 'GL', 'GR', 'JU', 'LU', 'NE', 'NW', 'OW', 'SG', 'SH', 'SO', 'SZ', 'TG', 'TI', 'UR', 'VD', 'VS', 'ZG', 'ZH', 'FL', 'CH'];
 const population = {
